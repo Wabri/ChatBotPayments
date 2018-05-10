89c28f92845c61420575b05c547877cd3ced5ad5
Wabri <gabriele.puliti@gmail.com>
2018-05-10 12:14:35 +0200

    set up the socket.io for the communication

--- BVChatBot/script/script.js
+++ BVChatBot/script/script.js
@@ -1,8 +1,11 @@
 // SpeechRecognition is the controller interface of the web speech api for voice recognition
 // SpeechRecognition (https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition)
-
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
+
+// Now use Socket.io for bidirectional communication between web clients and servers
+// Socket.io (https://socket.io/)
+const socket = io();
 
 // Properties of recognition
 recognition.lang = 'it-IT';
@@ -19,4 +22,21 @@ recognition.addEventListener('result', (e) => {
     let text = e.result[last][0].transcript;  // transform the audio result in text
 
     console.log('Confidence: ' + e.result[0][0].confidence);
+
+    // socket.emit register a new handler (text) for the given event ('chatMessage')
+    socket.emit('chatMessage', text);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
