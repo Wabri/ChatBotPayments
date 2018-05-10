1a9561337ea1c2efcbcb92e03f493cb6ce4b8aca
Wabri <gabriele.puliti@gmail.com>
2018-05-10 11:03:29 +0200

    init server and communication

---
+++ BVChatBot/index.js
@@ -0,0 +1,18 @@
+
+// Express is require to run chat in a local server
+// Express (https://expressjs.com/)
+
+// Creation of server
+const express = require('express');
+const app = express();
+
+// Set workdirectory of app
+app.use(express.static(__dirname));
+
+// Open port 5000
+const server = app.listen(5000);
+
+// When someone send a request to app then send index.html
+app.get('/', (req,res) => {
+    res.sendFile('index.html');
+});
