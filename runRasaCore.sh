c84a683b4be33e497128850314572bfc09904cda
Wabri <gabriele.puliti@gmail.com>
2018-07-11 15:00:15 +0200

    update

--- runRasaCore.sh
+++ runRasaCore.sh
@@ -1,7 +1,5 @@
 cd RASA_IA
 
-python2.7 -m spacy download it
-
 python2.7 -m rasa_nlu.train --config nluModelConfig.yml --data data/ --project current --fixed_model_name nlu --path models/
 
 python2.7 -m rasa_core.train -d domain.yml -s stories.md -o models/current/core --epochs 100
