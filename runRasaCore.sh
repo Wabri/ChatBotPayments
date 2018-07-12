a2b1912754168245201a5310c37650b808de9d0d
Wabri <gabriele.puliti@gmail.com>
2018-07-12 10:16:05 +0200

    update

--- runRasaCore.sh
+++ runRasaCore.sh
@@ -6,4 +6,4 @@ python2.7 -m rasa_nlu.train --config nluModelConfig.yml --data data/ --project c
 
 python2.7 -m rasa_core.train -d domain.yml -s stories.md -o models/current/core --epochs 100
 
-python2.7 -m rasa_core.server -d models/current/core -u models/current/nlu -o out.log -p 5004
+python2.7 -m rasa_core.server -d models/current/core -u models/current/nlu -o out.log -p 5004 --verbose
