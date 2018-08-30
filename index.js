59c70926e01bd77b882de477e156fff8bf4d430a
Wabri <gabriele.puliti@gmail.com>
2018-08-30 10:22:01 +0200

    update index

--- index.js
+++ index.js
@@ -16,51 +16,58 @@
                  * saranno definiti gli intenti e le entità della
                  * richiesta
                  */
-
                 if (messageReceive == "conversation reset " + userID) {
-                    try {
-                        // headers = "Content-Type", "application/json"
-                        // payload = "events":[{"event":"restart"}]
-                        var promise = $http.post("http://192.168.170.120:5004/conversations/" + socket.id + "/continue", payload)
-                            .then(function successCallback(response) {
-                                console.log("**** reset conversation ****");
-                                socket.emit("botResponse", "La conversazione è stata cancellata");
-                            });
-                    } catch (err) {
+                    $http({
+                        method: 'POST',
+                        url: "http://192.168.170.120:5004/conversations/" + socket.id + "/continue",
+                        data: $.param({
+                            events: [{"event": "restart"}],
+                        }),
+                        headers: {
+                            'Content-Type': 'application/json'
+                        }
+                    }).success(function (data, status, headers, config) {
+                        console.log("**** reset conversation ****");
+                        socket.emit("botResponse", "La conversazione è stata cancellata");
+                    }).error(function (data, status, headers, config) {
                         console.log("**** C'è stato un errore durante la chiamata verso rasa ****");
+                        console.log("errore: " + data);
+                        socket.emit("botResponse", "In questo momento il servizio non è attivo, riprovare più tardi!");
+                    });
+                } else {
+                    $http({
+                        method: 'POST',
+                        url: "http://192.168.170.120:5004/conversations/" + socket.id + "/parse",
+                        data: $.param({
+                            "query": "message"
+                        }),
+                        headers: {'Content-Type': 'application/json'}
+                    }).success(function (data, status, headers, config) {
+                        var arr = JSON.parse(data.text);
+                        console.log("Sender: " + arr.tracker["sender_id"]);
+                        console.log("next_action: " + arr.next_action);
+                        if (arr.next_action != "action_listen") {
+                            $http({
+                                method: 'POST',
+                                url: "http://192.168.170.120:5004/conversations/" + socket.id + "/respond",
+                                data: $.param({
+                                    "query": "message"
+                                }),
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                }
+                            }).success(function (data, status, headers, config) {
+                                var arrt = JSON.parse(response.text);
+                                console.log("botResponse: " + arrt[0].text);
+                                socket.emit("botResponse", arrt[0].text);
+                            }).error(function (data, status, headers, config) {
+                                socket.emit("botResponse", "Puoi cercare di essere più chiaro?");
+                            });
+                        }
+                    }).error(function (data, status, headers, config) {
                         console.log("errore: " + err);
                         socket.emit("botResponse", "In questo momento il servizio non è attivo, riprovare più tardi!");
-                        return;
-                    }
-                } else {
-                    // headers = "Content-Type", "application/json"
-                    // body = {"query":"message"}
-                    var promise = $http.post("http://192.168.170.120:5004/conversations/" + socket.id + "/parse", payload)
-                        .then(function successCallback(response) {
-                            try {
-                                var arr = JSON.parse(res.text);
-                                console.log("Sender: " + arr.tracker["sender_id"]);
-                                console.log("next_action: " + arr.next_action);
-                                if (arr.next_action != "action_listen") {
-                                    // headers = "Content-Type", "application/json"
-                                    // body = {"query":"message"}
-                                    var promise = $http.post("http://192.168.170.120:5004/conversations/" + socket.id + "/respond", payload)
-                                        .then(function successCallback(response) {
-                                            var arrt = JSON.parse(response.text);
-                                            console.log("botResponse: " + arrt[0].text);
-                                            socket.emit("botResponse", arrt[0].text);
-                                            return;
-                                        });
-                                } else {
-                                    socket.emit("botResponse", "Puoi cercare di essere più chiaro?");
-                                    return;
-                                }
-                            } catch (err) {
-                                console.log("errore: " + err);
-                                socket.emit("botResponse", "In questo momento il servizio non è attivo, riprovare più tardi!");
-                                return;
-                            }
-                        });
+                    });
                 }
             });
         });
