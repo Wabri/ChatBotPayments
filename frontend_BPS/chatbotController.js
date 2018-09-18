748c0dcc597794c6e5c315975a5c23bdaef44f18
Wabri <gabriele.puliti@gmail.com>
2018-09-18 14:57:06 +0200

    update frontend bps and wikiù

--- frontend_BPS/chatbotController.js
+++ frontend_BPS/chatbotController.js
@@ -25,6 +25,16 @@
     	speechSynthesis.speak(utterance);
     }
     
+    function voiceRecognition() {
+    	var recognizer = new speechRecognition();
+    	recognizer.continous = false; // questo serve per stoppare il riconoscimento quando l'utente smette di parlare
+    	recognizer.lang = "it-IT";
+    	
+    	recognizer.onresult = function(event) {
+    		return event.results[0][0].transcript;
+    	}
+    }
+    
     function sendUserMessage() {
 
       ChatbotService.sendMessageToRasa(id, $scope.inputMessage).then(function(response) {
@@ -43,27 +53,44 @@
     }
     
     function listenUserMessage() {
-    	annyang.addCommands(commands);
-    	annyang.debug();
-    	annyang.start();
+//    	annyang.addCommands(commands);
+//    	annyang.debug();
+//    	annyang.start();
+    	
+    	$scope.botMessage = voiceRecognition();
+    	
+    	ChatbotService.sendMessageToRasa(id, $scope.inputMessage).then(function(response) {
+            $scope.userMessage = $scope.inputMessage;
+            $scope.inputMessage = "";
+            ChatbotService.reciveMessageFromRasa(id,$scope.inputMessage).then(function(response) {
+                if (response.data[0].text != null) {
+                	$scope.botMessage = response.data[0].text;
+                	voiceSynth($scope.botMessage);
+                } else {
+                	$scope.botMessage = "Errore";
+                }
+            }, function() {});
+          }, function() {});
+    	
     }
     
-    var commands = {
-    		'*message' : function(message) {
-    			console.log(message);
-    			$scope.userMessage = message;
-    			ChatbotService.sendMessageToRasa(id, $scope.userMessage).then(function(response) {
-    		        ChatbotService.reciveMessageFromRasa(id, $scope.userMessage).then(function(response) {
-    		            if (response.data[0].text != null) {
-    		            	$scope.botMessage = response.data[0].text;
-    		            	voiceSynth($scope.botMessage);
-    		            } else {
-    		            	$scope.botMessage = "Errore";
-    		            }
-    		        }, function() {});
-    		      }, function() {});
-    		}
-    }
+//    usato da annyang prima di scoprire che c'era il riconoscimento vocale integrato 
+//    var commands = {
+//    		'*message' : function(message) {
+//    			console.log(message);
+//    			$scope.userMessage = message;
+//    			ChatbotService.sendMessageToRasa(id, $scope.userMessage).then(function(response) {
+//    		        ChatbotService.reciveMessageFromRasa(id, $scope.userMessage).then(function(response) {
+//    		            if (response.data[0].text != null) {
+//    		            	$scope.botMessage = response.data[0].text;
+//    		            	voiceSynth($scope.botMessage);
+//    		            } else {
+//    		            	$scope.botMessage = "Errore";
+//    		            }
+//    		        }, function() {});
+//    		      }, function() {});
+//    		}
+//    }
 
   }
 
